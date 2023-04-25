4fe5ca0118d6f600d9af98c98dd886b9691616b8
MaxWang <964032176@qq.com>
2023-04-25 22:04:14 +0800

    Store module/package.json

--- pages/learn/[db]/index.tsx
+++ pages/learn/[db]/index.tsx
@@ -2,14 +2,44 @@
  * 数据库主页，根据数据库种类 [db] 从后端拉取不同的内容。
  */
 
-import React, { ReactElement } from "react";
-import { Grid, Stack } from "@mui/material";
+import React, { ReactElement, useEffect } from "react";
+import {
+  Collapse,
+  Grid,
+  List,
+  ListItem,
+  ListItemButton,
+  ListItemIcon,
+  ListItemText,
+  Stack,
+  Typography
+} from "@mui/material";
 import Header from "@/components/header/Header";
 import Subheader from "@/components/header/Subheader";
 import AppGridLayout from "@/layouts/AppGridLayout";
 import databases from "@/contents/databases";
+import { useRouter } from "next/router";
+import axios from "axios";
+import { ExpandLess, ExpandMore, StarBorder } from "@mui/icons-material";
 
 export default function Learn() {
+  const [data,setData] = React.useState();
+  const router = useRouter();
+  const {db} = router.query;
+  const [open,setOpen] = React.useState(true);
+  useEffect(()=>{
+    axios.get("https://api.petclinic.homans.world:8443/listDisease/")
+      .then(response =>{
+        if(response.data.error_num ===1){
+          alert(response.data.msg);
+        }else {
+        setData(response.data);
+          console.log(response.data);
+        }
+      },error =>{
+        console.log(error);
+      })
+  },[])
   return (
     <>
       <Grid item xs={12}>
@@ -22,7 +52,34 @@ export default function Learn() {
       </Grid>
 
       <Grid item xs={3}>
-        {/* list */}
+        <List>
+          {data?.map((item,i)=>(
+            <box>
+              <ListItemButton onClick={()=>{
+                console.log(item.category);
+                //router.push(`/learn/${db}/`);
+                setOpen(!open);
+
+              }}>
+                <ListItemText primary={item.category}/>
+                {open ? <ExpandLess /> : <ExpandMore />}
+              </ListItemButton>
+              <Collapse in={open} timeout="auto" unmountOnExit>
+                <List component="div" disablePadding>
+                  <ListItemButton onClick={()=>{
+                    router.push(`/learn/${db}/${i}`);
+                  }} >
+                    <ListItemText primary={item.disease_name}></ListItemText>
+                  </ListItemButton>
+
+
+                </List>
+              </Collapse>
+            </box>
+
+
+            ))}
+        </List>
       </Grid>
 
       <Grid item xs={9}>
