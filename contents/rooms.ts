6cee3c63688284e03ed234431f17f1bb841d8d9a
FWLJCL <3106886964@qq.com>
2023-04-28 04:05:53 +0800

    Update responsibility data again

--- contents/rooms.ts
+++ contents/rooms.ts
@@ -9,11 +9,11 @@ const rooms: RoomBriefType[] = [
     panoMarkers: [
       {
         id: "processing-paperwork",
-        tooltip: "处理文件工作",
+        tooltip: "门诊室",
         image: "/marker.png",
         size: { width: 32, height: 32 },
-        position: { yaw: "45deg", pitch: "0deg" },
-        data: { link: "/tour/reception/processing-paperwork" }
+        position: { yaw: "500deg", pitch: "20deg" },
+        data: { link: "/tour/outpatient-room" }
       },
     ],
   },
@@ -31,12 +31,12 @@ const rooms: RoomBriefType[] = [
     panoSrc: "/pano/outpatient-room.jpg",
     panoMarkers:  [
       {
-        id: "medical-consulting",
-        tooltip: "医学问诊",
+        id: "to-pharmacy",
+        tooltip: "药房",
         image: "/marker.png",
         size: { width: 32, height: 32 },
         position: { yaw: "45deg", pitch: "0deg" },
-        data: { link: "/tour/outpatient-room/medical-consulting" }
+        data: { link: "/tour/pharmacy" }
       },
     ],
   },
@@ -112,11 +112,29 @@ const rooms: RoomBriefType[] = [
     panoMarkers:  [
       {
         id: "get-medicine",
-        tooltip: "取药",
+        tooltip: "药品介绍",
         image: "/marker.png",
         size: { width: 32, height: 32 },
-        position: { yaw: "45deg", pitch: "0deg" },
+        position: { yaw: "1000deg", pitch: "0deg" },
+        data: { link: "/tour/pharmacy/prescription-medicine-label" }
+      },
+
+      {
+        id: "get",
+        tooltip: "待命名标签1",
+        image: "/marker.png",
+        size: { width: 32, height: 32 },
+        position: { yaw: "1300deg", pitch: "0deg" },
         data: { link: "/tour/pharmacy/get-medicine" }
+      },
+
+      {
+        id: "guiding-pharmacy",
+        tooltip: "指导用药",
+        image: "/marker.png",
+        size: { width: 32, height: 32 },
+        position: { yaw: "1500deg", pitch: "0deg" },
+        data: { link: "/tour/pharmacy/guiding-pharmacy" }
       },
     ],
   },
@@ -124,7 +142,7 @@ const rooms: RoomBriefType[] = [
     title: "输液室",
     subtitle: "infusion-room",
     // panoSrc: "/pano/infusion-room.jpg",
-    panoSrc: "/pano/test.jpg",
+    panoSrc: "/pano/infusion-room.jpg",
     panoMarkers: null,
   },
   {
