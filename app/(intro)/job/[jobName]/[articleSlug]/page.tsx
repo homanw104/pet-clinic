9f6194939628490462431124fdfcd940ac31f416
Homan <homanwang@hotmail.com>
2025-11-11 18:24:11 +0800

    Generate metadata for job articles

--- app/(intro)/job/[jobName]/[articleSlug]/page.tsx
+++ app/(intro)/job/[jobName]/[articleSlug]/page.tsx
@@ -1,6 +1,7 @@
 import { getArticleBySlug, getArticleSlugs } from "@/utils/article_util";
 import PageContent from "@/app/(intro)/job/[jobName]/[articleSlug]/page-content";
 import jobs from "@/contents/jobs";
+import { Metadata } from "next";
 
 export async function generateStaticParams() {
   let paramsList: { jobName: string; articleSlug: string }[] = [];
@@ -16,6 +17,22 @@ export async function generateStaticParams() {
   }
 
   return paramsList;
+}
+
+export async function generateMetadata({ params }: {
+  params: Promise<{
+    jobName: string;
+    articleSlug: string;
+  }>;
+}): Promise<Metadata> {
+  const paramsData = await params;
+  let title = paramsData.articleSlug.replace("-", " ");
+  title = title.charAt(0).toUpperCase() + title.slice(1) + " | Pet Clinic";
+
+  return {
+    title: title,
+    description: title
+  };
 }
 
 export default async function Page({ params }: {
